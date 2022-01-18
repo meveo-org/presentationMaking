bb23f9627e3592236bfd5ecb7c1fa9b77497b34b
smichea <smichea@gmail.com>
2022-01-18 03:04:15 +0100

    added 3 new videos

--- tts.sh
+++ tts.sh
@@ -4,7 +4,7 @@ echo "Call google tts service."
 curl -X POST \
 -H "Authorization: Bearer "$(gcloud auth application-default print-access-token) \
 -H "Content-Type: application/json; charset=utf-8" \
--d @cloneAndRunMeveoLocally/cloneAndRunMeveoLocallyF.json \
+-d @installModuleFromGitRepo/installModuleFromGitRepo7.json \
 "https://texttospeech.googleapis.com/v1/text:synthesize" > json_response.json
 
 
@@ -12,4 +12,4 @@ echo "Extract base64 audio content."
 jq  -r .audioContent json_response.json > audio_base64.txt
 
 echo "Decode base64 to mp3."
-base64 audio_base64.txt -d > cloneAndRunMeveoLocally/cloneAndRunMeveoLocallyF.mp3
+base64 audio_base64.txt -d > installModuleFromGitRepo/installModuleFromGitRepo7.mp3
